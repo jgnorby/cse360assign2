d241d40b5474d1fac50be405dda72c98e0d466d3
Jen Norby <jgnorby@asu.edu>
2019-02-22 22:34:57 -0700

    Added method body to each method.

--- cse360assign2/src/cse360assign2/Calculator.java
+++ cse360assign2/src/cse360assign2/Calculator.java
@@ -1,39 +1,70 @@
 package cse360assign2;
 
 /**
- * Calculator.java
  * @author	Jennifer Norby
  * @version	February 22, 2019
  */
 
+/*
+ * A simple calculator class that adds, subtracts, multiplies, divides,
+ * and returns the history
+ */
 public class Calculator {
 	
 	private int total;
 	
+	/**
+	 * Constructor	
+	 */
 	public Calculator () {
 		total = 0;  // not needed  - included for clarity
 	}
 	
+	/**
+	 * @return total
+	 */
 	public int getTotal () {
-		return 0;
+		return total;
 	}
 	
+	/**
+	 * Addition method - add value to total
+	 * @param value
+	 */
 	public void add (int value) {
-		
+		total += value;
 	}
 	
+	/**
+	 * Subtraction method - subtracts value from total
+	 * @param value
+	 */
 	public void subtract (int value) {
-		
+		total -= value;
 	}
 	
+	/**
+	 * Multiplication method - multiplies total by the value
+	 * @param value
+	 */
 	public void multiply (int value) {
-		
+		total *= value;
 	}
 	
+	/**
+	 * Division method - divides total by the value
+	 * @param value
+	 */
 	public void divide (int value) {
-		
+		if ( value == 0 )
+			total = 0;
+		else
+			total /= value;
 	}
 	
+	/**
+	 * @return history
+	 */
 	public String getHistory () {
 		return "";
 	}
