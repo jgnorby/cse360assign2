b3e2586b44d8c0223c3c4a317fde1b6be9a1a3f0
Jen Norby <jgnorby@asu.edu>
2019-02-22 23:00:23 -0700

    Added history to each method.

--- cse360assign2/src/cse360assign2/Calculator.java
+++ cse360assign2/src/cse360assign2/Calculator.java
@@ -12,18 +12,22 @@ package cse360assign2;
 public class Calculator {
 	
 	private int total;
+	private String history;
 	
 	/**
 	 * Constructor	
 	 */
 	public Calculator () {
+		
 		total = 0;  // not needed  - included for clarity
+		history = "";
 	}
 	
 	/**
 	 * @return total
 	 */
 	public int getTotal () {
+		
 		return total;
 	}
 	
@@ -32,7 +36,12 @@ public class Calculator {
 	 * @param value
 	 */
 	public void add (int value) {
+		
 		total += value;
+		
+		if ( history == "" )
+			history += "0";
+		history = history + " + " + value;
 	}
 	
 	/**
@@ -40,7 +49,12 @@ public class Calculator {
 	 * @param value
 	 */
 	public void subtract (int value) {
+		
 		total -= value;
+		
+		if ( history == "" )
+			history += "0";
+		history = history + " - " + value;
 	}
 	
 	/**
@@ -48,7 +62,12 @@ public class Calculator {
 	 * @param value
 	 */
 	public void multiply (int value) {
+		
 		total *= value;
+		
+		if ( history == "" )
+			history += "0";
+		history = history + " * " + value;
 	}
 	
 	/**
@@ -56,16 +75,22 @@ public class Calculator {
 	 * @param value
 	 */
 	public void divide (int value) {
+		
 		if ( value == 0 )
 			total = 0;
 		else
 			total /= value;
+		
+		if ( history == "" )
+			history += "0";
+		history = history + " / " + value;
 	}
 	
 	/**
 	 * @return history
 	 */
 	public String getHistory () {
-		return "";
+		
+		return history;
 	}
 }
